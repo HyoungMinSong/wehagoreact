0b91d38afc7cd769dc1e505f3fc15600369a2519
HyoungMinSong <shmin11@naver.com>
2023-07-14 12:57:22 +0900

    회원가입UI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
 import SignUp_infomation from './pages/signUp/SignUp_infomation';
+import SignUp_company from './pages/signUp/SignUp_company';
+import SignUp_complete from './pages/signUp/SignUp_complete';
+
 
 
 
@@ -10,8 +13,14 @@ function App() {
     <div className="App">
       <p>하이~~</p>
       <Link to={'/signup'}>회원가입으로 가기</Link>
+      <br/>
+      <Link to={'/signup/company'}>회원가입/회사정보입력으로 가기</Link>
+      <br/>
+      <Link to={'/signup/complete'}>회원가입/회원가입완료로 가기</Link>
       <Routes>
         <Route path='/signup' element={<SignUp_infomation/>}/>
+        <Route path='/signup/company' element={<SignUp_company/>}/>
+        <Route path='/signup/complete' element={<SignUp_complete/>}/>
         <Route path="/detail" element={ <div>라우트 테스트임</div> } />
         <Route path="/about" element={ <div>라우트 테스트임2222</div> } />
     </Routes>
--- src/pages/signUp/SignUp_company.js
+++ src/pages/signUp/SignUp_company.js
@@ -4,7 +4,7 @@ import InputMask from 'react-input-mask';
 
 
 // className="border-danger"
-function SignUp_infomation() {
+function SignUp_company() {
   let [name, setName] = useState('');
   let [nameError, setNameError] = useState(false);
   let [phoneNumber, setPhoneNumber] = useState('');
@@ -29,10 +29,10 @@ function SignUp_infomation() {
         <div className="horizontal-line "></div>
         
         <Card.Body className="text-center h2">
-          <Card.Title>WEHAGO 회원정보입력</Card.Title>
-          <div className="horizontal-line2"></div>
+          <Card.Title>신규 회사 만들기</Card.Title>
+          <div className="horizontal-line3"></div>
           <Card.Text className="text-muted small-text mt-3">
-          회원님의 정보를 입력해주세요.
+          회원님의 회사 정보를 입력해주세요.
 
           </Card.Text>
        
@@ -42,22 +42,31 @@ function SignUp_infomation() {
         <div className="text-start">
       <Form.Group className="my-2" controlId="formBasicName">
       
-        <Form.Label style={{ fontWeight: 'bold' }}>사용자 이름</Form.Label>
+        <Form.Label style={{ fontWeight: 'bold' }}>회사 이름</Form.Label>
       
-        <Form.Control type="email" placeholder="Enter name"  onChange={(e)=>{
+        <Form.Control type="text" placeholder="Enter company name"  onChange={(e)=>{
           setName(e.target.value);
           regex.test(name) ? setNameError(false) : setNameError(true);
           }} className={ nameError ? "border-danger" : ""}/>
         
       </Form.Group>
+      <Form.Group className="my-2" controlId="formBasicName">
       
+        <Form.Label style={{ fontWeight: 'bold' }}>구분</Form.Label>
+      <Form.Select aria-label="Default select example">
+      <option>Select business type</option>
+      <option value="1">개인사업자</option>
+      <option value="2">법인사업자</option>
+      <option value="3">기타</option>
+    </Form.Select>
+    </Form.Group>
       <Form.Group className="my-2 " controlId="formBasicPhone">
-                  <Form.Label style={{ fontWeight: 'bold' }}>휴대전화번호</Form.Label>
+                  <Form.Label style={{ fontWeight: 'bold' }}>사업자등록번호</Form.Label>
                   <InputMask
-                    mask="999-9999-9999"
+                    mask="999-99-99999"
                     maskChar="_"
                     // className=""
-                    placeholder="Enter phone number"
+                    placeholder="Enter business registration number"
                     onChange={(e)=>{
                       setPhoneNumber(e.target.value);
                       numberRegex.test(phoneNumber) ? setphoneNumberError(false) : setphoneNumberError(true);
@@ -72,49 +81,48 @@ function SignUp_infomation() {
                 </Form.Group>
                 
                 <Form.Group className="my-2" controlId="formBasicEmail" >
-        <Form.Label style={{ fontWeight: 'bold' }}>아이디</Form.Label>
-        <Form.Control type="email" placeholder="Enter id"  onChange={(e)=>{
+        <Form.Label style={{ fontWeight: 'bold' }}>업태</Form.Label>
+        <Form.Control type="text" placeholder="Enter business status"  onChange={(e)=>{
                       setId(e.target.value);}}/>
-        <Form.Text className="text-muted" style={{ fontSize: '11px'}} >
-        * 생성하신 아이디는 변경할수없으니 가입하시는 본인의 아이디로 생성하시기 바랍니다.
-
-        </Form.Text>
       </Form.Group>
               
       <Form.Group className="my-2" controlId="formBasicPassword" >
       
-        <Form.Label style={{ fontWeight: 'bold' }}>Password</Form.Label>
+        <Form.Label style={{ fontWeight: 'bold' }}>업종</Form.Label>
         
-        <Form.Control type="password" placeholder="Password" onChange={(e)=>{
+        <Form.Control type="text" placeholder="Enter business category" onChange={(e)=>{
                       setPassword(e.target.value);}}/>
-        <Form.Text className="text-muted" style={{ fontSize: '11px'}} >
-        * 비밀번호는 8자~16자의 영문 대문자, 소문자, 숫자 및 특수문자 중 2가지 이상의 조합으로
-입력해주세요
-
-        </Form.Text>
       </Form.Group>
       
       <Form.Group className="my-2" controlId="formBasicPassword2" >
-        <Form.Label style={{ fontWeight: 'bold' }}>Confirm password</Form.Label>
-        <Form.Control type="password" placeholder="Confirm Password" onChange={(e)=>{
+        <Form.Label style={{ fontWeight: 'bold' }}>대표자 이름</Form.Label>
+        <Form.Control type="text" placeholder="Enter representative name" onChange={(e)=>{
                       setConfirmPassword(e.target.value);}}/>
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail" >
-        <Form.Label style={{ fontWeight: 'bold' }}>이메일 주소</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" onChange={(e)=>{
-                      setEmail(e.target.value);}}/>
-        <Form.Text className="text-muted" style={{ fontSize: '11px'}} >
-        * 아이디, 비밀번호 찾기 등 본인확인이 필요한 경우 사용할 이메일 주소입니다.
-
-        </Form.Text>
-      </Form.Group>
+      <Form.Group className="my-2 " controlId="formBasicPhone">
+                  <Form.Label style={{ fontWeight: 'bold' }}>회사 전화번호</Form.Label>
+                  <InputMask
+                    mask="999-9999-9999"
+                    maskChar="_"
+                    // className=""
+                    placeholder="Enter company phone numberr"
+                    onChange={(e)=>{
+                      setPhoneNumber(e.target.value);
+                      numberRegex.test(phoneNumber) ? setphoneNumberError(false) : setphoneNumberError(true);
+                      }} className={ phoneNumberError ? "form-control border-danger" : "form-control"}
+                           
+                    
+                  />
+                </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
       </Form.Group>
       </div>
       <div className="pt-3 pb-2 text-muted" style={{ backgroundColor: '#F8FFFF', fontSize: '11px' }}>
-      <p>- 회사/단체 이름은 가입완료 후 변경할 수 없으므로 정확한 정보를 입력하세요.</p>
-      <p>- 가입완료 후 회사설정에서 회사인증 절차를 완료하면 더 다양한 서비스를 이용하실 수 있습니다.</p>
+      <p>- 회사이름, 구분 등 기본정보는 입력 후 변경할 수 없으므로 정확한 정보를 입력하세요.
+</p>
+      <p>- 기업회원가입 후 회사관리설정 메뉴에서 회사인증을 통해 인증해 실제 사업자임을 인증하고 사용하실 수 있습니다.
+</p>
       </div>
       <div className="my-4">
       <Button variant="light" type="submit" className="mx-2">
@@ -135,4 +143,4 @@ function SignUp_infomation() {
     );
 }
 
-export default SignUp_infomation;
+export default SignUp_company;
