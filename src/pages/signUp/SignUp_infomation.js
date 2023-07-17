4762098c6406551511862edfab2dd0677b2908dd
HyoungMinSong <shmin11@naver.com>
2023-07-17 17:33:57 +0900

    5:33

--- src/pages/signUp/SignUp_infomation.js
+++ src/pages/signUp/SignUp_infomation.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Form, Modal, Row } from "react-bootstrap";
 import InputMask from 'react-input-mask';
+import axiosApi from "../../AxiosApi";
 
 
 // className="border-danger"
@@ -20,30 +21,37 @@ function SignUp_infomation() {
 
   let regex = /^[가-힣a-zA-Z]+$/;
   let numberRegex = /^\d{2,3}-\d{3,4}-\d{4}$/;
-  let idRegex = /^[a-z]+[a-z0-9]{5,19}$/g;
+  // let idRegex = /^[a-z]+[a-z0-9]{5,19}$/g;
+  let idRegex = /^[a-zA-Z0-9]*$/;
   let pwRegex = /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/;
   let emailRegex = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
 
   useEffect(() => {
-    if(name != ''){
-    regex.test(name) ? setNameError(false) : setNameError(true);
+    if (name != '') {
+      regex.test(name) ? setNameError(false) : setNameError(true);
     }
-    if(phoneNumber != ''){
-    numberRegex.test(phoneNumber) ? setphoneNumberError(false) : setphoneNumberError(true);
+    if (phoneNumber != '') {
+      numberRegex.test(phoneNumber) ? setphoneNumberError(false) : setphoneNumberError(true);
     }
-    if(id != ''){
-    idRegex.test(id) ? setIdError(false) : setIdError(true);
+    if (id != '') {
+      idRegex.test(id) ? setIdError(false) : setIdError(true);
     }
-    if(password != ''){
-    pwRegex.test(password) ? setPasswordError(false) : setPasswordError(true);
+    if (password != '') {
+      pwRegex.test(password) ? setPasswordError(false) : setPasswordError(true);
     }
-    if(confirmPassword != ''){
-    password === confirmPassword ? setConfirmPasswordError(false) : setConfirmPasswordError(true);
-  }
-    if(email != ''){
-    emailRegex.test(email) ? setEmailError(false) : setEmailError(true);
-}
-  }, [name, phoneNumber, id, password,confirmPassword, email]);
+    if (confirmPassword != '') {
+      password === confirmPassword ? setConfirmPasswordError(false) : setConfirmPasswordError(true);
+    }
+    if (email != '') {
+      emailRegex.test(email) ? setEmailError(false) : setEmailError(true);
+    }
+  }, [name, phoneNumber, id, password, confirmPassword, email]);
+
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
 
@@ -77,12 +85,12 @@ function SignUp_infomation() {
                         setName(e.target.value);
                         // regex.test(name) ? setNameError(false) : setNameError(true);
                       }} className={nameError ? "border-danger" : ""} />
-{nameError? 
-  <Form.Text className="text-danger" style={{ fontSize: '11px' }}   >
-                        한글과 영문만 입력 가능합니다.
-                      </Form.Text>
-                      : ""
-}
+                      {nameError ?
+                        <Form.Text className="text-danger" style={{ fontSize: '11px' }}   >
+                          한글과 영문만 입력 가능합니다.
+                        </Form.Text>
+                        : ""
+                      }
                     </Form.Group>
 
                     <Form.Group className="my-2 " controlId="formBasicPhone">
@@ -99,17 +107,17 @@ function SignUp_infomation() {
 
 
                       />
-{phoneNumberError? 
-  <Form.Text className="text-danger" style={{ fontSize: '11px' }}   >
-                        휴대전화번호 11자리를 입력하세요.
-                      </Form.Text>
-                      : <Form.Text className="text-muted" style={{ fontSize: '11px' }}   >
-                      * 입력된 휴대전화번호는 아이디, 비밀번호 찾기 등 본인 확인 용도 또는 WEHAGO로부터 알림
-                      을 받을 때 사용됩니다.
-                    </Form.Text>
-}
+                      {phoneNumberError ?
+                        <Form.Text className="text-danger" style={{ fontSize: '11px' }}   >
+                          휴대전화번호 11자리를 입력하세요.
+                        </Form.Text>
+                        : <Form.Text className="text-muted" style={{ fontSize: '11px' }}   >
+                          * 입력된 휴대전화번호는 아이디, 비밀번호 찾기 등 본인 확인 용도 또는 WEHAGO로부터 알림
+                          을 받을 때 사용됩니다.
+                        </Form.Text>
+                      }
 
-                      
+
                     </Form.Group>
 
                     <Form.Group className="my-2" controlId="formBasicEmail" >
@@ -117,17 +125,17 @@ function SignUp_infomation() {
                       <Form.Control type="text" placeholder="Enter id" onChange={(e) => {
                         setId(e.target.value);
                         // idRegex.test(id) ? setIdError(false) : setIdError(true);
-                      }} className={idError ? "border-danger" : ""}/>
-                      {idError? 
-  <Form.Text className="text-danger" style={{ fontSize: '11px' }}   >
-                        영문자로 시작하는 영문자 또는 숫자 6~20자를 입력하세요.
-                      </Form.Text>
-                      : <Form.Text className="text-muted" style={{ fontSize: '11px' }} >
-                      * 생성하신 아이디는 변경할수없으니 가입하시는 본인의 아이디로 생성하시기 바랍니다.
+                      }} className={idError ? "border-danger" : ""} />
+                      {idError ?
+                        <Form.Text className="text-danger" style={{ fontSize: '11px' }}   >
+                          영문자로 시작하는 영문자 또는 숫자 6~20자를 입력하세요.
+                        </Form.Text>
+                        : <Form.Text className="text-muted" style={{ fontSize: '11px' }} >
+                          * 생성하신 아이디는 변경할수없으니 가입하시는 본인의 아이디로 생성하시기 바랍니다.
 
-                    </Form.Text>
-}
-                      
+                        </Form.Text>
+                      }
+
                     </Form.Group>
 
                     <Form.Group className="my-2" controlId="formBasicPassword" >
@@ -138,17 +146,17 @@ function SignUp_infomation() {
                         setPassword(e.target.value);
                         // pwRegex.test(password) ? setPasswordError(false) : setPasswordError(true);
                       }} className={passwordError ? "border-danger" : ""} />
-                      {passwordError? 
-  <Form.Text className="text-danger" style={{ fontSize: '11px' }}   >
-                        8 ~ 16자 영문, 숫자, 특수문자를 최소 한가지씩 조합해서 입력하세요.
-                      </Form.Text>
-                      : <Form.Text className="text-muted" style={{ fontSize: '11px' }} >
-                      * 비밀번호는 8자~16자의 영문 대문자, 소문자, 숫자 및 특수문자 중 2가지 이상의 조합으로
-                      입력해주세요
+                      {passwordError ?
+                        <Form.Text className="text-danger" style={{ fontSize: '11px' }}   >
+                          8 ~ 16자 영문, 숫자, 특수문자를 최소 한가지씩 조합해서 입력하세요.
+                        </Form.Text>
+                        : <Form.Text className="text-muted" style={{ fontSize: '11px' }} >
+                          * 비밀번호는 8자~16자의 영문 대문자, 소문자, 숫자 및 특수문자 중 2가지 이상의 조합으로
+                          입력해주세요
 
-                    </Form.Text>
-}
-                      
+                        </Form.Text>
+                      }
+
                     </Form.Group>
 
                     <Form.Group className="my-2" controlId="formBasicPassword2" >
@@ -156,30 +164,30 @@ function SignUp_infomation() {
                       <Form.Control type="password" placeholder="Confirm Password" onChange={(e) => {
                         setConfirmPassword(e.target.value);
                         // password === confirmPassword ? setConfirmPasswordError(false) : setConfirmPasswordError(true);
-                      }}className={confirmPasswordError ? "border-danger" : ""} />
-                      {confirmPasswordError? 
-  <Form.Text className="text-danger" style={{ fontSize: '11px' }}   >
-                        비밀번호가 일치하지 않습니다.
-                      </Form.Text>
-                      : ""
-}
+                      }} className={confirmPasswordError ? "border-danger" : ""} />
+                      {confirmPasswordError ?
+                        <Form.Text className="text-danger" style={{ fontSize: '11px' }}   >
+                          비밀번호가 일치하지 않습니다.
+                        </Form.Text>
+                        : ""
+                      }
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail" >
                       <Form.Label style={{ fontWeight: 'bold' }}>이메일 주소</Form.Label>
                       <Form.Control type="email" placeholder="Enter email" onChange={(e) => {
                         setEmail(e.target.value);
                         // emailRegex.test(email) ? setEmailError(false) : setEmailError(true);
-                      }} className={emailError ? "border-danger" : ""}/>
-                      {emailError? 
-  <Form.Text className="text-danger" style={{ fontSize: '11px' }}   >
-                        이메일 형식이 아닙니다.
-                      </Form.Text>
-                      : <Form.Text className="text-muted" style={{ fontSize: '11px' }} >
-                      * 아이디, 비밀번호 찾기 등 본인확인이 필요한 경우 사용할 이메일 주소입니다.
+                      }} className={emailError ? "border-danger" : ""} />
+                      {emailError ?
+                        <Form.Text className="text-danger" style={{ fontSize: '11px' }}   >
+                          이메일 형식이 아닙니다.
+                        </Form.Text>
+                        : <Form.Text className="text-muted" style={{ fontSize: '11px' }} >
+                          * 아이디, 비밀번호 찾기 등 본인확인이 필요한 경우 사용할 이메일 주소입니다.
 
-                    </Form.Text>
-}
-                      
+                        </Form.Text>
+                      }
+
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
                       <Form.Check type="checkbox" label="Check me out" />
@@ -193,22 +201,49 @@ function SignUp_infomation() {
                     <Button variant="light" type="submit" className="mx-2">
                       &lt; 이전
                     </Button>
-                    <Button variant="primary" className="mx-3" onClick={() => { 
-                      
+                    <Button variant="primary" className="mx-3" onClick={() => {
+                      if (name != '' && nameError == false && phoneNumber != '' && phoneNumberError == false && id != ''
+                        && idError == false && password != '' && passwordError == false && confirmPassword != '' && confirmPasswordError == false &&
+                        email != '' && emailError == false) {
+                        axiosApi.post("/idcheck", {
+                          id: id
+                        }).then((c) => {
+                          if (c.data === id) {
+                            handleShow();
+                          }
+                          console.log(c.data);
+                        }).catch(() => {
+
+                          console.log('실패함')
+                        })
+                      }
                       // console.log(name, phoneNumber, id, password, confirmPassword, email) 
-                     
-                      
-                      }}>
+
+
+                    }}>
                       다음 &gt;
                     </Button>
                   </div>
                 </Form>
               </Container>
-
             </Card>
           </div>
         </Col>
       </Row>
+      <Modal show={show} onHide={handleClose} style={{ color: "black" }}>
+        <Modal.Header closeButton>
+          <Modal.Title>회원가입 에러</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>중복된 ID입니다</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          {/* <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button> */}
+        </Modal.Footer>
+      </Modal>
     </Container>
   );
 }
