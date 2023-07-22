b77ba7da85a5a299c94a91d9abec63984d229941
Cometary <109892844+Cometary@users.noreply.github.com>
2023-07-22 14:17:23 +0900

    Feat: 조직도 수정 완성

--- src/index.js
+++ src/index.js
@@ -38,11 +38,11 @@ root.render(
         </Route>
         <Route path="/about" element={ <div>라우트 테스트임2222</div> } />
         <Route path="/login" element={ <div className="d-flex align-items-center py-4 bg-body-tertiary">
-        <LoginPage></LoginPage></div>} />
-<Route path="/findId" element={ <FindIdForm></FindIdForm> } />
-<Route path="/findpw" element={ <FindPwForm></FindPwForm> } />
-<Route path="/findidresult" element={ <FindIdResult></FindIdResult> } />
-<Route path="/findpwresult" element={ <FindpwResult></FindpwResult> } />
+          <LoginPage></LoginPage></div>} />
+        <Route path="/findId" element={ <FindIdForm></FindIdForm> } />
+        <Route path="/findpw" element={ <FindPwForm></FindPwForm> } />
+        <Route path="/findidresult" element={ <FindIdResult></FindIdResult> } />
+        <Route path="/findpwresult" element={ <FindpwResult></FindpwResult> } />
       </Routes>
   </BrowserRouter>
   </Provider>
