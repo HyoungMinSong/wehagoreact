732257fb65395307222b0db156db0dc4313f89ad
HyoungMinSong <shmin11@naver.com>
2023-07-28 17:58:24 +0900

    레인지 버튼 추가중, 직원초대 페이지 추가중, 직원초대 api 생성

--- src/index.js
+++ src/index.js
@@ -25,6 +25,8 @@ import SignUpHeader from './pages/signUp/SignUpHeader';
 import Updatepw from './pages/login/Updatepw';
 import LoginRoute from './LoginRoute';
 import SendTest from './pages/signUp/SendTest';
+import SignUpRedirect from './pages/signUp/SignUpRedirect';
+import SignUp_invite from './pages/signUp/SignUp_invite';
 
 const getCookie = (name) => {
   const value = `; ${document.cookie}`;
@@ -60,6 +62,9 @@ root.render(
         <Route path="/test" element={ <Test_up></Test_up>} />
         <Route path="/test2" element={ <SignUpHeader></SignUpHeader>} />
         <Route path="/test3" element={ <SendTest></SendTest>} />
+        <Route path="/s/:shortLink" element={ <SignUpRedirect></SignUpRedirect> }/>
+        <Route path="/signup/invite" element={ <SignUp_invite></SignUp_invite>} />
+
       </Routes>
     </BrowserRouter>
   </Provider>
