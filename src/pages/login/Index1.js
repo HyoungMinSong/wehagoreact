c7b2894cdef888aa92c0c1cc125e89411d3a024e
jongwon0801 <choijong0801@naver.com>
2023-08-09 14:28:55 +0900

    업데이트성공

--- src/pages/login/Index1.js
+++ src/pages/login/Index1.js
@@ -156,28 +156,13 @@ const IndexBackground = () => {
                 </div>
               </div>
             </div>
-            <div className="App">
-      <p>하이~~</p>
-      <Link to={'/signup'}>회원가입으로 가기</Link>
-      <br/>
-      <Link to={'/signup/company'}>회원가입/회사정보입력으로 가기</Link>
-      <br/>
-      <Link to={'/signup/complete'}>회원가입/회원가입완료로 가기</Link>
-      <br/>
-      <Link to={'/signup/package'}>회원가입/가입패키지결제로 가기</Link>
-      <br/>
-      <Link to={'/login'}>로그인으로 가기</Link>
-      <br />
-      <Link to={'/organization/management'}>조직도로 가기</Link>
-      <br/>
-      <Link to={'/main'}>메인화면으로 가기</Link>
+            
     </div>
             <div style={{background: "#333948"}}>
         <Footer/>
       </div>
           </div>
         </div>
-      </div>
     </>
     
   );
