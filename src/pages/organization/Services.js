e2d272a10706b4b11ae6d17fac509b974738e8b3
HyoungMinSong <shmin11@naver.com>
2023-08-18 09:07:28 +0900

    서비스 배포 체크박스 넣는중

--- src/pages/organization/Services.js
+++ src/pages/organization/Services.js
@@ -5,6 +5,10 @@ import axiosApi from "../../AxiosApi";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import Swal from "sweetalert2";
+import { Button, Checkbox, FormControlLabel } from "@mui/material";
+import SendIcon from '@mui/icons-material/Send';
+
+
 
 
 function Services() {
@@ -18,7 +22,36 @@ function Services() {
   let [publishedUserList, setPublishedUserList] = useState('');
   let [checkedService, setCheckedService] = useState(0);
   let [showListAfterConfirm, setShowListAfterConfirm] = useState(false);
-  let [stateAssociatedConfirm, setStateAssociatedConfirm] =useState(false);
+  let [stateAssociatedConfirm, setStateAssociatedConfirm] = useState(false);
+  // 하위 체크박스의 개수만큼의 초기 상태 배열 생성
+  const [checkboxStates, setCheckboxStates] = useState([]);
+  let [countCheckBoxArray, setCountCheckBoxArray] = useState([]);
+
+  // useEffect(() => {
+  //   setCountCheckBox(checkboxStates.reduce((count, value) => count + value, 0)); // 예시: countA의 두 배 값을 countB로 설정
+  // }, [checkboxStates]);
+
+    useEffect(() => {
+    setCountCheckBoxArray(checkboxStates.map((element, index) => element === true ? index : undefined).filter(index => index !== undefined)); // 예시: countA의 두 배 값을 countB로 설정
+  }, [checkboxStates]);
+
+  // 상위 체크박스의 선택 여부 계산
+  const isParentChecked = checkboxStates.every((isChecked) => isChecked);
+  const isParentIndeterminate = checkboxStates.some((isChecked) => isChecked) && !isParentChecked;
+  console.log("isParentChecked : " + isParentChecked);
+  console.log("isParentIndeterminate : " + isParentIndeterminate);
+  // 상위 체크박스 선택 여부 변경 시 처리
+  const handleParentChange = (event) => {
+    const newCheckboxStates = checkboxStates.map(() => event.target.checked);
+    setCheckboxStates(newCheckboxStates);
+  };
+
+  // 하위 체크박스 선택 여부 변경 시 처리
+  const handleChildChange = (index) => (event) => {
+    const newCheckboxStates = [...checkboxStates];
+    newCheckboxStates[index] = event.target.checked;
+    setCheckboxStates(newCheckboxStates);
+  };
 
   // 로그인 유저 정보
   const loginedUser = useSelector((state) => state.loginUserData);
@@ -30,38 +63,38 @@ function Services() {
 
   useEffect(() => {
     // return () => {
-      showCompanySerivces();
+    showCompanySerivces();
   }, [show, loginedUser.companyName])
   // 모달창 띄우기 유무
 
-const showCompanySerivces = async() => {
-  setLoading(true);
-  if (!show && !stateAssociatedConfirm) {
-    
-    await axiosApi.post("/findservicelistbycomno", {
-      comNo: comNo
-    }).then((c) => {
-      setCompanyServices(c.data);
-      console.log(c.data);
-    }).catch(() => { console.log('실패실패') });
-    await axiosApi.post("/findpackagecount", {
-      comNo: comNo
-    }).then((c) => {
-      setPackageCount(c.data);
-      console.log(c.data);
-    }).catch(() => { console.log('실패실패2') })
-    // }
+  const showCompanySerivces = async () => {
+    setLoading(true);
+    if (!show && !stateAssociatedConfirm) {
+
+      await axiosApi.post("/findservicelistbycomno", {
+        comNo: comNo
+      }).then((c) => {
+        setCompanyServices(c.data);
+        console.log(c.data);
+      }).catch(() => { console.log('실패실패') });
+      await axiosApi.post("/findpackagecount", {
+        comNo: comNo
+      }).then((c) => {
+        setPackageCount(c.data);
+        console.log(c.data);
+      }).catch(() => { console.log('실패실패2') })
+      // }
+    }
+    setLoading(false);
   }
-  setLoading(false);
-}
 
   useEffect(() => {
     if (showListAfterConfirm) {
       console.log("이건가 ?")
       getUnpublishedUser(comNo, checkedService);
       getPublishedUser(comNo, checkedService);
-    }return ()=>{
-      
+    } return () => {
+
 
 
     }
@@ -73,6 +106,7 @@ const showCompanySerivces = async() => {
     }).then((c) => {
       console.log(c.data);
       setUnPublishedUserList(c.data);
+      setCheckboxStates(new Array(c.data.length).fill(false));
     }).catch(() => {
       console.log('실패실패3');
     })
@@ -105,13 +139,13 @@ const showCompanySerivces = async() => {
       if (result.isConfirmed) {
         // 만약 모달창에서 confirm 버튼을 눌렀다면
         axiosApi.post("/saveinvitedemployeepublish", {
-          serviceNo: cs, employeeNo: en, comNo:comNo, packCt : packageCount
+          serviceNo: cs, employeeNo: en, comNo: comNo, packCt: packageCount
         }).then((c) => {
-          if(c.data === 0){
-          console.log(c.data);
-          setShowListAfterConfirm(true);
-          setStateAssociatedConfirm(false);
-          }else if(c.data ===1 ){
+          if (c.data === 0) {
+            console.log(c.data);
+            setShowListAfterConfirm(true);
+            setStateAssociatedConfirm(false);
+          } else if (c.data === 1) {
             console.log("와이")
             Swal.fire({
               icon: 'error',
@@ -151,7 +185,7 @@ const showCompanySerivces = async() => {
           empNo: en, serviceNo: cs
         }).then((c) => {
           console.log(c.data);
-          
+
           setShowListAfterConfirm(true);
           setStateAssociatedConfirm(false);
         }).catch(() => {
@@ -160,6 +194,52 @@ const showCompanySerivces = async() => {
       }
     });
   }
+
+  let confirmArrayPublish = () => {
+    Swal.fire({
+      title: "총" + countCheckBoxArray.length + "명에게 " + serviceName + "를 배포하시겠습니까 ?",
+      text: "승인을 누르면 해당 직원들에게 서비스가 배포됩니다.",
+      icon: "warning",
+      showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
+      confirmButtonColor: "#3085d6", // confrim 버튼 색깔 지정
+      cancelButtonColor: "#d33", // cancel 버튼 색깔 지정
+      confirmButtonText: "승인", // confirm 버튼 텍스트 지정
+      cancelButtonText: "취소", // cancel 버튼 텍스트 지정
+      // reverseButtons: true, // 버튼 순서 거꾸로
+    })
+    .then((result) => {
+      // 만약 Promise리턴을 받으면,
+      if (result.isConfirmed) {
+        // 만약 모달창에서 confirm 버튼을 눌렀다면
+        let newArray = countCheckBoxArray.map(index => unPublishedUserList[index].empNo);
+        axiosApi.post("/savearrayinvitedemployeepublish", {
+          serviceNo: checkedService, arrayEmployeeNo: newArray, comNo: comNo, packCt: packageCount, totalAddEmployeeCount : countCheckBoxArray.length
+        }).then((c) => {
+          if (c.data === 0) {
+            console.log(c.data);
+            setShowListAfterConfirm(true);
+            setStateAssociatedConfirm(false);
+          } else if (c.data === 1) {
+            console.log("와이")
+            Swal.fire({
+              icon: 'error',
+              title: '배포 실패',
+              text: '구매한 패키지를 초과하셨습니다.',
+            });
+          } else {
+            Swal.fire({
+              icon: 'error',
+              title: '배포 실패',
+              text: '서비스 배포에 실패했습니다.',
+            });
+          }
+        }).catch(() => {
+          console.log('실패실패3');
+        })
+      }
+    });
+  }
+
 
   return (
     <div >
@@ -171,77 +251,43 @@ const showCompanySerivces = async() => {
         <div className="my-5" >
           <Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1">
             <Row >
-              {/* <Col sm={4}>
-                <ListGroup>
-                  <ListGroup.Item action href="#link1">
-                    {loginedUser.companyName}
-                  </ListGroup.Item>
-                  <ListGroup.Item action href="#link2">
-                  &nbsp;
-                  </ListGroup.Item>
-                  <ListGroup.Item action href="#link3">
-                  &nbsp;
-                  </ListGroup.Item>
-                  <ListGroup.Item action href="#link4">
-                  &nbsp;
-                  </ListGroup.Item>
-                  <ListGroup.Item action href="#link5">
-                  &nbsp;
-                  </ListGroup.Item>
-                  <ListGroup.Item action href="#link6">
-                  &nbsp;
-                  </ListGroup.Item>
-                  <ListGroup.Item action href="#link7">
-                  &nbsp;
-                  </ListGroup.Item>
-                  <ListGroup.Item action href="#link8">
-                  &nbsp;
-                  </ListGroup.Item>
-                  <ListGroup.Item action href="#link9">
-                  &nbsp;
-                  </ListGroup.Item>
-                  <ListGroup.Item action href="#link10">
-                  &nbsp;
-                  </ListGroup.Item>
-                </ListGroup>
-              </Col>
-              <Col sm={8}> */}
-                <Tab.Content>
-                  <Tab.Pane eventKey="#link1">
-                    <div className="row g-4  row-cols-1 row-cols-lg-5">
-                      {companySerivces &&
-                        companySerivces.map(function (a) {
-                          return (<div className=" col " >
-                            <div style={{ backgroundColor: '#f5f7f9' }} className="py-5 rounded " onClick={() => {
+              <Tab.Content>
+                <Tab.Pane eventKey="#link1">
+                  <div className="row g-4  row-cols-1 row-cols-lg-5">
+                    {companySerivces &&
+                      companySerivces.map(function (a) {
+                        return (<div className=" col " >
+                          <div style={{ backgroundColor: '#f5f7f9' }} className="py-5 rounded " onClick={() => {
 
-                              setServiceName(a.serviceName);
-                              setCheckedService(a.serviceNo);
-                              setPublishedUserList('')
-                              setUnPublishedUserList('')
-                              getUnpublishedUser(comNo, a.serviceNo);
-                              getPublishedUser(comNo, a.serviceNo);
-                              setShowListAfterConfirm(false);
-                              setStateAssociatedConfirm(true);
-                              setShow(true);
-                            }}>
-                              <div className="feature-icon d-inline-flex align-items-center justify-content-center  bg-gradient fs-2 mb-3">
-                                {/* <CategoryOutlinedIcon style={{ fontSize: '80px' }} />
+                            setServiceName(a.serviceName);
+                            setCheckedService(a.serviceNo);
+                            setPublishedUserList('')
+                            setUnPublishedUserList('')
+                            getUnpublishedUser(comNo, a.serviceNo);
+
+                            getPublishedUser(comNo, a.serviceNo);
+                            setShowListAfterConfirm(false);
+                            setStateAssociatedConfirm(true);
+                            setShow(true);
+                          }}>
+                            <div className="feature-icon d-inline-flex align-items-center justify-content-center  bg-gradient fs-2 mb-3">
+                              {/* <CategoryOutlinedIcon style={{ fontSize: '80px' }} />
                            */}
-                                <img src={a.serviceMainIconPath} alt="서비스 이미지" width="80px" height="80px" style={{ borderRadius: '12px' }} />
-                              </div>
-                              <h3 className="fs-2 text-body-emphasis">{a.serviceName}</h3>
-                              <p>사용자의 수 [<span className="text-primary">{a.count}</span>/{packageCount}]</p>
-                              <a href="#" className="icon-link">
-                                Call to action
-                                &gt;
-                              </a>
+                              <img src={a.serviceMainIconPath} alt="서비스 이미지" width="80px" height="80px" style={{ borderRadius: '12px' }} />
                             </div>
-                          </div>)
-                        })
-                      }
-                    </div></Tab.Pane>
-                  <Tab.Pane eventKey="#link2">Tab pane content 2</Tab.Pane>
-                </Tab.Content>
+                            <h3 className="fs-2 text-body-emphasis">{a.serviceName}</h3>
+                            <p>사용자의 수 [<span className="text-primary">{a.count}</span>/{packageCount}]</p>
+                            <a href="#" className="icon-link">
+                              Call to action
+                              &gt;
+                            </a>
+                          </div>
+                        </div>)
+                      })
+                    }
+                  </div></Tab.Pane>
+                <Tab.Pane eventKey="#link2">Tab pane content 2</Tab.Pane>
+              </Tab.Content>
               {/* </Col> */}
             </Row>
           </Tab.Container>
@@ -264,6 +310,16 @@ const showCompanySerivces = async() => {
               <Table bordered hover>
                 <thead>
                   <tr>
+                    <th><FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={isParentChecked}
+                          indeterminate={isParentIndeterminate}
+                          onChange={handleParentChange}
+                        />
+                      }
+
+                    /></th>
                     <th>이름</th>
                     <th>아이디</th>
                     <th>소속</th>
@@ -272,11 +328,21 @@ const showCompanySerivces = async() => {
                 </thead>
                 <tbody>
                   {unPublishedUserList &&
-                    unPublishedUserList.map(function (a) {
+                    unPublishedUserList.map(function (a, index) {
                       return (
                         <tr onClick={() => {
                           confirmPublish(serviceName, a.userName, checkedService, a.empNo)
                         }}>
+                          <td onClick={(e) => e.stopPropagation()}><FormControlLabel
+                            key={index}
+                            control={
+                              <Checkbox
+                                checked={checkboxStates[index]}
+                                onChange={handleChildChange(index)}
+                              />
+                            }
+
+                          /></td>
                           <td>{a.userName}</td>
                           <td>{a.userId}</td>
                           <td>{a.empDuty}</td>
@@ -288,6 +354,15 @@ const showCompanySerivces = async() => {
                 </tbody>
               </Table>
             </Modal.Body>
+            {(isParentChecked || isParentIndeterminate) && (
+              <Modal.Footer>
+                <Button variant="contained" endIcon={<SendIcon />} onClick={()=>{
+confirmArrayPublish();
+                }}>
+                  {countCheckBoxArray.length}명 배포하기
+                </Button>
+              </Modal.Footer>
+            )}
           </Tab>
           <Tab eventKey="profile" title="배포 해제">
             <Modal.Body>
@@ -309,17 +384,18 @@ const showCompanySerivces = async() => {
                           confirmUnPublish(serviceName, a.userName, checkedService, a.empNo);
                         }}>
 
-                            <td>{a.userName}</td>
-                            <td>{a.userId}</td>
-                            <td>{a.empDuty}</td>
-                            <td>{a.empPosition}</td>
-                          </tr>
-                          )
+                          <td>{a.userName}</td>
+                          <td>{a.userId}</td>
+                          <td>{a.empDuty}</td>
+                          <td>{a.empPosition}</td>
+                        </tr>
+                      )
                     })
                   }
-                        </tbody>
+                </tbody>
               </Table>
             </Modal.Body>
+
           </Tab>
 
         </Tabs>
@@ -328,8 +404,8 @@ const showCompanySerivces = async() => {
 
       </Modal>
       {loading && (
-            <div className="overlay-loading-box text-center">
-        
+        <div className="overlay-loading-box text-center">
+
           {/* 로딩 스피너 컴포넌트 */}
           <Spinner animation="border" variant="primary" style={{ fontSize: '3rem', width: "6rem", height: "6rem" }} />
           <div className="mt-3">서비스 배포 목록이 출력 중입니다.<br />잠시만 기다려주세요.</div>
--- src/pages/signUp/TestChat3.js
+++ src/pages/signUp/TestChat3.js
@@ -1,23 +1,73 @@
 import React, { useState } from 'react';
-import Chatbot from 'react-chatbot-kit'
-import 'react-chatbot-kit/build/main.css'
-import config from './config';
-import MessageParser from './MessageParser';
-import ActionProvider from './ActionProvider';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
-  const TestChat3 = () => {
-    return (
-      <div>
-        <Chatbot
-          config={config}
-          messageParser={MessageParser}
-          actionProvider={ActionProvider}
-        />
-      </div>
-    );
+function TestChat3() {
+  const [checked, setChecked] = useState([false,false,false,false,false,false,false,false,false]);
+  const [indeterminate, setIndeterminate] = useState(false);
+
+  const handleCheck = () => {
+    if (!checked) {
+      setChecked(true);
+    } else if (indeterminate) {
+      setIndeterminate(false);
+    } else {
+      setChecked(false);
+      setIndeterminate(true);
+    }
   };
-  
 
+  return (
+    <div>
+<FormControlLabel
+  label="Parent"
+  control={
+    <Checkbox
+      checked={checked[0] && checked[1]}
+      indeterminate={checked[0] !== checked[1]}
+
+    />
+  }
+/>
+<FormControlLabel
+        label="Child 2"
+        control={<Checkbox checked={checked[0]}  />}
+      />
+      <FormControlLabel
+        label="Child 2"
+        control={<Checkbox checked={checked[1]}  />}
+      />
+      <FormControlLabel
+        label="Child 2"
+        control={<Checkbox checked={checked[2]}  />}
+      />
+      <FormControlLabel
+        label="Child 2"
+        control={<Checkbox checked={checked[3]}  />}
+      />
+      <FormControlLabel
+        label="Child 2"
+        control={<Checkbox checked={checked[4]}  />}
+      />
+      <FormControlLabel
+        label="Child 2"
+        control={<Checkbox checked={checked[5]}  />}
+      />
+      <FormControlLabel
+        label="Child 2"
+        control={<Checkbox checked={checked[6]}  />}
+      />
+      <FormControlLabel
+        label="Child 2"
+        control={<Checkbox checked={checked[7]}  />}
+      />
+      <FormControlLabel
+        label="Child 2"
+        control={<Checkbox checked={checked[8]}  />}
+      />
+
+    </div>
+  );
+}
 
 export default TestChat3;
-
